a625cace56d26e70eb3fc2bde136956cb542dac8
Amy Bertken <amy.bertken@gmail.com>
2015-02-09 16:03:13 -0700

    create page with list of events; user has ability to create new event

---
+++ app/controllers/events_controller.rb
@@ -0,0 +1,23 @@
+class EventsController < ApplicationController
+  def index
+    @events = Event.all
+  end
+
+  def new
+    @event = Event.new
+  end
+
+  def create
+    @event = Event.new (event_params)
+    if @event.save
+      redirect_to events_path
+    else
+      render :new
+    end
+  end
+
+  def event_params
+    params.require(:event).permit(:date, :location, :description, :capacity, :requires_id)
+  end
+
+end
